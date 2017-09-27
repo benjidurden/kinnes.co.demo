54d79ed3ee8151e68532c974cc6207f68ed0036f
benjidurden <benjidurdenn@gmail.com>
2017-09-27 07:04:09 -0400

    content first draft

--- src/Home.js
+++ src/Home.js
@@ -23,17 +23,17 @@ class Home extends Component {
             <h1 className="title">Brian Kinnes</h1>
             <h3 className="catalog" onMouseOver = {this.rollPhoto}>Photo</h3>
                 <div className={this.state.closedPhoto ? 'invisible': 'visible'}>
-                    <p className="portfolioItem"><Link to = "/redmercedes/">Red Mercedes</Link></p>
-                    <p className="portfolioItem"><Link to = "/heebiejeebies/">Heebiejeebies</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/redmercedes/">Red Mercedes</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/heebiejeebies/">Heebiejeebies</Link></p>
                 </div>
             <h3 className="catalog2" onMouseOver = {this.rollFilm}>Film</h3>
                 <div className={this.state.closedFilm ? 'invisible': 'visible'}>
-                    <p className="portfolioItem"><Link to = "/caroline/">Caroline</Link></p>
-                    <p className="portfolioItem"><Link to = "/myneighbor/">My Neighbor is a Drug Dealer</Link></p>
-                    <p className="portfolioItem"><Link to ="/wendy/">Wendy</Link></p>
-                    <p className="portfolioItem"><Link to = "/american-boyband/">American Boyband</Link></p>
-                    <p className="portfolioItem"><Link to = "/helmet-boy/">Helmet Boy EP.04</Link></p>
-                    <p className="portfolioItem"><Link to = "/beauty-salon/">Beauty Salon</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/caroline/">Caroline</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/myneighbor/">My Neighbor is a Drug Dealer</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to ="/wendy/">Wendy</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/american-boyband/">American Boyband</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/helmet-boy/">Helmet Boy Episode 4</Link></p>
+                    <p className="portfolioItem"><Link className="removeStyling" to = "/beauty-salon/">Beauty Salon</Link></p>
                 </div>
             <h3><a className="link" href="http://blog.briankinn.es">Blog</a></h3>
             <h3><Link className="storeLink" to="/store/">Store</Link></h3>
--- src/PhotoFinal.js
+++ src/PhotoFinal.js
@@ -13,19 +13,25 @@ class PhotoFinal extends Component {
     // }
     render(){
         var currentPhoto = null;
+        var currentTitle = null;
+        var currentDescription = null;
         var currentPath = this.props.location.pathname;
         if (currentPath === "/redmercedes/"){
+            currentTitle = <h4>AMINÉ - REDMERCEDES</h4>;
+            currentDescription = <h4>SINGLE</h4>;
             currentPhoto = <img src="https://res.cloudinary.com/benjidurden/image/upload/v1506458553/b2fcba2db9cc8eace656aa81500c005e.1000x936x1_leg1om.jpg" className="aminePhoto" alt="Red Mercedes Cover" />;
         }
         else if (currentPath === "/heebiejeebies/"){
+            currentTitle = <h4>AMINÉ FEAT. KEHLANI - HEEBIEJEEBIES</h4>; 
+            currentDescription = <h4>SINGLE</h4>;
             currentPhoto = <img src="https://res.cloudinary.com/benjidurden/image/upload/v1506458549/akeemofzamunda-amine-kehlani-heebiejeebies-0_rzl3bz.jpg" className="aminePhoto" alt="HeebieJeebies Cover"/>;
         }
 
         return (
             <div>
                 {/* Goal: Use conditional rendering to render the content of the page based on the page location */}
-            <h1>Photo Final Page</h1>
-            <p>Add photo title and description property</p>
+            {currentTitle}
+            {currentDescription}
             {currentPhoto}
             </div>
         )
