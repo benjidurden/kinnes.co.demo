7acb5300ace0198e9267f527f099c1bf9e1d4f4a
benjidurden <benjidurdenn@gmail.com>
2017-10-02 15:56:30 -0400

    first push to now

--- src/Store.js
+++ src/Store.js
@@ -2,45 +2,26 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import FaShoppingCart from "react-icons/lib/fa/shopping-cart/";
 
-const title = [
-    'Photo Prints ',
-    'Support Brian Kinnes Shirt'
-];
-
-
 class Store extends Component {
     render(){
         return (
-            <div>
+           <div>
+            <h2><Link to = "/">Home</Link></h2>
             <h1 className="storeHead"><i><Link to = "/store/">Store</Link></i></h1>
-            <div>
             <Link to = "/store/cart/">
-            <FaShoppingCart size={31}/>
+            <FaShoppingCart className = "cartIcon" size={31}/>
             </Link>
-            </div>
-             <div id = "allItemWrapper">
-             <div className="itemContainer">
+             <div className = "itemContainer">
+             <img className="prints" src="https://res.cloudinary.com/benjidurden/image/upload/v1505945857/DJkqrrPVwAAtDzm_vlsgbf.jpg" alt="Prints" />
              <Link to = "/store/prints/">
-             <img className="prints mainPage" src="https://res.cloudinary.com/benjidurden/image/upload/v1505945857/DJkqrrPVwAAtDzm_vlsgbf.jpg" alt="Prints" />
-             <div className = "textWrapper">
-             <div className="text">
-             {title}
-             </div>
-             </div>
+             Photo Prints
              </Link>
              </div>
-             <div id = "spacing">
-             </div>
-             <div className="itemContainer">
-            <Link to = "/store/support-bk-shirt/">
-             <img className="shirt-pic mainPage" src="https://res.cloudinary.com/benjidurden/image/upload/v1505946146/shirt_v4_dichtv.png" alt="Support Brian Kinnes" />
-             <div className = "textWrapper">
-             <div className="text">
-             {title}
-             </div>
-             </div>
+             <div className = "itemContainer">
+             <img className="shirt-pic" src="https://res.cloudinary.com/benjidurden/image/upload/v1505946146/shirt_v4_dichtv.png" alt="Support Brian Kinnes" />
+             <Link to = "/store/support-bk-shirt/">
+             Support Brian Kinnes Shirt
              </Link>
-             </div>
              </div>
             </div>
         )
