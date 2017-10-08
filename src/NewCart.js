7eeead0164dfce9777459a870b39e66dacba1dc8
benjidurden <benjidurdenn@gmail.com>
2017-10-08 08:15:54 -0400

    render worked! rendering prices now.

--- src/NewCart.js
+++ src/NewCart.js
@@ -5,6 +5,7 @@ import {InputNumber, Button, Modal, Input, Radio, Menu, Dropdown, Icon} from 'an
 import ConfirmPurchase from './ConfirmPurchase';
 import storage from './storage';
 import FaShoppingCart from "react-icons/lib/fa/shopping-cart/";
+import EmptyCart from './EmptyCart.js';
 //After cart image - what comes next?
 
 class NewCart extends Component {
@@ -100,21 +101,102 @@ class NewCart extends Component {
         //Using storage to render the right carts
         if (storage.hasShirtInCart && storage.hasPrintsInCart){
         console.log("You'd like to purchase some shirts and prints");
+        cartRender = 
+        <div>
+        <h1><Link className="homeHead" to = "/"><i>Home</i></Link></h1>
+        <FaShoppingCart id="newCartIcon" className = "cartIcon" size={31}/>
+        <h2 id="cartIdentifier"><i>Your Cart</i></h2>
+        <hr className="hr--small" />
+        <div className="row cart_row">
+            <div className="col-sm-3">
+                <p className="tableHead" id="quantityDesc"><b>Product</b></p>
+            </div>
+            <div className="col-sm-3">
+                <p className="tableHead" id="quantityDesc"><b>Info</b></p>
+            </div>
+            <div className="col-sm-3">
+                <p className="tableHead" id="quantityDesc"><b>Quantity</b></p>
+            </div>
+            <div className="col-sm-3">
+                <p className="tableHead" id="totalDesc"><b>Total</b></p>
+            </div>
+            {/* With Bootstrap do all 12 columns have to be used? */}
+        </div>
+        <br />
+        <div className="row item_row">
+        {/* Image, followed by item name, description, and remove */}
+        {/* This is a test of the two items, more-so myself figuring out what CSS they're going to need when they get rendered */}
+            <div className="col-sm-3">
+            <img id="cartShirt" src="https://res.cloudinary.com/benjidurden/image/upload/v1507282288/shirt_front_nn1ek1.png" alt="shirtInCart" />
+            </div>
+            <div className="col-sm-3">
+            <Link to = "/store/support-bk-shirt/"> <p className="itemTitle"><i>Support Brian Kinnes Shirt</i></p></Link>
+            {/* Shirt size. */}
+            <p className="itemSize">SMALL</p>
+            <p className="itemRemover" onClick={this.removeItem}>Remove</p>
+            </div>
+            <div className="col-sm-3">
+                <InputNumber min={2} max={10}  onChange={this.handleChange} />
+            </div>
+            <div className="col-sm-3">
+                <p className="costContainer"><b><span>$</span>{shirtPrice}
+                </b></p>
+            </div>
+        </div>
+        <div className="row item_row">
+            <div className="col-sm-3">
+            <img id="cartPrint"
+            src="https://res.cloudinary.com/benjidurden/image/upload/v1507282301/prints_front_rlexvr.png"
+            alt="printInCart" />
+            </div>
+            <div className="col-sm-3">
+            <Link to = "/store/prints/"><p className="itemTitle"><i>Photo Prints</i></p></Link>
+            <p className="itemRemover" onClick={this.removeItem}>Remove</p>
+            </div>
+            <div className="col-sm-3">
+                <InputNumber min={2} max={10}  onChange={this.handleChange} />
+            </div>
+            <div className="col-sm-3">
+                <p className="costContainer"><b><span>$</span>
+                {printPrice}
+                </b></p>
+            </div>
+        </div>
+        <br />
+        <div className="row borderRow">
+        {/* an HR then a subtotal, shipping, update cart and checkout */}
+        {/* How much spacing is necessary? I think that two 4-sized columns would work, followed by the payment buttons. */}
+        <p id="subTotal" className="costContainer">
+            <label htmlFor="subTotal">SubTotal:   </label>
+            <b><span>$</span>30</b></p>
+        <p id="shipping" className="costContainer">
+            <label htmlFor="shipping">Shipping: </label>
+            <b><span>$ </span>20</b></p>
+        </div>
+        {/* Cart Info. Pass price to confirmPurchase onclick */}
+        <Button size="default" onClick={this.changeCart}>Update Cart</Button>
+        <Button className={this.state.buttonFirstClick ? "cartButtons": "hideModal"} size={this.state.size} onClick={this.showModal}>
+            Checkout
+            </Button>
+            {stripe_checkout_init}
+           <Modal
+            title="Sure you're not a robot?"
+            visible={this.state.visible}
+            onOk = {this.handleOk}
+            onCancel={this.handleCancel}
+            confirmLoading={this.state.confirmLoading}
+            okText="Okay"
+            cancelText="Cancel"
+             >
+        <Input placeholder="Enter your full name" name={this.state.fullName} />
+        <p>Just a failsafe! Hit the Checkout button again after you type this in.</p>
+        </Modal>
+        </div>
         }
         else if (storage.hasPrintsInCart){
         console.log("You'd like to only purchase a print");
-        }
-        else if(storage.hasShirtInCart){
-        console.log("You'd like to only purchase a shirt");
-        }
-        //Default, if the cart is empty pass this 
-        else {
-        console.log("You want to purchase nothing");
-        }
-
-        return (
-            <div className="container">
-            <h1><Link className="homeHead" to = "/"><i>Home</i></Link></h1>
+        cartRender = <div>
+        <h1><Link className="homeHead" to = "/"><i>Home</i></Link></h1>
             <FaShoppingCart id="newCartIcon" className = "cartIcon" size={31}/>
             {/* Cart Icon on right */}
             <h2 id="cartIdentifier"><i>Your Cart</i></h2>
@@ -135,26 +217,6 @@ class NewCart extends Component {
                 {/* With Bootstrap do all 12 columns have to be used? */}
             </div>
             <br />
-            <div className="row item_row">
-            {/* Image, followed by item name, description, and remove */}
-            {/* This is a test of the two items, more-so myself figuring out what CSS they're going to need when they get rendered */}
-                <div className="col-sm-3">
-                <img id="cartShirt" src="https://res.cloudinary.com/benjidurden/image/upload/v1507282288/shirt_front_nn1ek1.png" alt="shirtInCart" />
-                </div>
-                <div className="col-sm-3">
-                <Link to = "/store/support-bk-shirt/"> <p className="itemTitle"><i>Support Brian Kinnes Shirt</i></p></Link>
-                {/* Shirt size. */}
-                <p className="itemSize">SMALL</p>
-                <p className="itemRemover" onClick={this.removeItem}>Remove</p>
-                </div>
-                <div className="col-sm-3">
-                    <InputNumber min={2} max={10}  onChange={this.handleChange} />
-                </div>
-                <div className="col-sm-3">
-                    <p className="costContainer"><b><span>$</span>{shirtPrice}
-                    </b></p>
-                </div>
-            </div>
             <div className="row item_row">
                 <div className="col-sm-3">
                 <img id="cartPrint"
@@ -203,6 +265,91 @@ class NewCart extends Component {
             <Input placeholder="Enter your full name" name={this.state.fullName} />
             <p>Just a failsafe! Hit the Checkout button again after you type this in.</p>
             </Modal>
+        </div>
+        }
+        else if(storage.hasShirtInCart){
+        console.log("You'd like to only purchase a shirt");
+        cartRender = <div>
+            <h1><Link className="homeHead" to = "/"><i>Home</i></Link></h1>
+            <FaShoppingCart id="newCartIcon" className = "cartIcon" size={31}/>
+            {/* Cart Icon on right */}
+            <h2 id="cartIdentifier"><i>Your Cart</i></h2>
+            <hr className="hr--small" />
+            <div className="row cart_row">
+                <div className="col-sm-3">
+                    <p className="tableHead" id="quantityDesc"><b>Product</b></p>
+                </div>
+                <div className="col-sm-3">
+                    <p className="tableHead" id="quantityDesc"><b>Info</b></p>
+                </div>
+                <div className="col-sm-3">
+                    <p className="tableHead" id="quantityDesc"><b>Quantity</b></p>
+                </div>
+                <div className="col-sm-3">
+                    <p className="tableHead" id="totalDesc"><b>Total</b></p>
+                </div>
+                {/* With Bootstrap do all 12 columns have to be used? */}
+            </div>
+            <br />
+            <div className="row item_row">
+            {/* Image, followed by item name, description, and remove */}
+            {/* This is a test of the two items, more-so myself figuring out what CSS they're going to need when they get rendered */}
+                <div className="col-sm-3">
+                <img id="cartShirt" src="https://res.cloudinary.com/benjidurden/image/upload/v1507282288/shirt_front_nn1ek1.png" alt="shirtInCart" />
+                </div>
+                <div className="col-sm-3">
+                <Link to = "/store/support-bk-shirt/"> <p className="itemTitle"><i>Support Brian Kinnes Shirt</i></p></Link>
+                {/* Shirt size. */}
+                <p className="itemSize">SMALL</p>
+                <p className="itemRemover" onClick={this.removeItem}>Remove</p>
+                </div>
+                <div className="col-sm-3">
+                    <InputNumber min={2} max={10}  onChange={this.handleChange} />
+                </div>
+                <div className="col-sm-3">
+                    <p className="costContainer"><b><span>$</span>{shirtPrice}
+                    </b></p>
+                </div>
+            </div>
+            <div className="row borderRow">
+            {/* an HR then a subtotal, shipping, update cart and checkout */}
+            {/* How much spacing is necessary? I think that two 4-sized columns would work, followed by the payment buttons. */}
+            <p id="subTotal" className="costContainer">
+                <label htmlFor="subTotal">SubTotal:   </label>
+                <b><span>$</span>30</b></p>
+            <p id="shipping" className="costContainer">
+                <label htmlFor="shipping">Shipping: </label>
+                <b><span>$ </span>20</b></p>
+            </div>
+            {/* Cart Info. Pass price to confirmPurchase onclick */}
+            <Button size="default" onClick={this.changeCart}>Update Cart</Button>
+            <Button className={this.state.buttonFirstClick ? "cartButtons": "hideModal"} size={this.state.size} onClick={this.showModal}>
+                Checkout
+                </Button>
+                {stripe_checkout_init}
+               <Modal
+                title="Sure you're not a robot?"
+                visible={this.state.visible}
+                onOk = {this.handleOk}
+                onCancel={this.handleCancel}
+                confirmLoading={this.state.confirmLoading}
+                okText="Okay"
+                cancelText="Cancel"
+                 >
+            <Input placeholder="Enter your full name" name={this.state.fullName} />
+            <p>Just a failsafe! Hit the Checkout button again after you type this in.</p>
+            </Modal>
+        </div>
+        }
+        //Default, if the cart is empty pass this 
+        else {
+        console.log("You want to purchase nothing");
+        cartRender = <EmptyCart />
+        }
+
+        return (
+            <div className="container">
+            {cartRender}
             </div>
         )
     }
